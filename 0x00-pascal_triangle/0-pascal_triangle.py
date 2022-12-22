f4d87d94f07ad8299dcd65fa1cc630486ad5894c
ashuhg <ashenafihagos.ah@gmail.com>
2022-12-23 01:40:36 +0300

    interview questinier

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -6,12 +6,24 @@
 def pascal_triangle(n):
     if n <= 0:
         return []
-    final_list = [[1]]
-     for x in range(n-1):
-         temp = [0] + final_list[-1] + [0]
-         new_list = []
-         for y in range(len(final_list[-1]) + 1):
-            new_list.append(temp[y] + temp[y+1])
-        final_list.append(new_list)
-    return final_list
+    for line in range(0, n) :
+         
+        # Every line has number of
+        # integers equal to line
+        # number
+        for i in range(0, line + 1) :
+            print(binomialCoeff(line, i),
+                " ", end = "")
+        print()
+     
+ 
 
+def binomialCoeff(n, k) :
+    res = 1
+    if (k > n - k) :
+        k = n - k
+    for i in range(0 , k) :
+        res = res * (n - i)
+        res = res // (i + 1)
+     
+    return res
